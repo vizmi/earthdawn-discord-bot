a397ffa42ddb409bb263e710048aaa01847a4c51
Mihaly Vizhanyo <vizhanyo.mihaly@gmail.com>
2024-12-21 14:20:16 -0700

    change dice logic

--- commands/rolling/roll.js
+++ commands/rolling/roll.js
@@ -1,46 +1,45 @@
 const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
+const data = require('./data.js');
 
-const localResponses = { 
-	criticalGlitch: { en: 'critical glitch', hu: 'kritikus hiba' },
-	glitch: { en: 'hit(s) with glitch', hu: 'találat hibával' },
-	hits: { en: 'hit(s)', hu: 'találat' },
-	reroll: { en: 'Reroll with Edge', hu: 'Újradobom Mázlival' },
-	keep: { en: 'Keep', hu: 'Megtartom' }
-}
-
-const roll_die = (sixes) => {
-	if (!sixes) return [ Math.floor(Math.random() * 6) + 1 ];
+const roll_die = (size) => {
 	const rolls = []
-	let roll = Math.floor(Math.random() * 6) + 1;
-	while (roll == 6) {
-		rolls.push(roll);
-		roll = Math.floor(Math.random() * 6) + 1;
+	let r = Math.floor(Math.random() * size) + 1;
+	while (r == size) {
+		rolls.push(r);
+		r = Math.floor(Math.random() * size) + 1;
 	}
-	rolls.push(roll);
+	rolls.push(r);
 	return rolls;
 }
 
-const roll = (dice, sixes) => {
-	const rolls = []
-	for (let i = 0; i < dice; i++) {
-		rolls.push(...roll_die(sixes));
-	}
+const roll_dice = (dice) => {
+	const rolls = [];
+	// iterate over dice
+	dice.forEach(sizeCount => {
+		const [size, count] = sizeCount;
+		for (let i = 0; i < count; i++) {
+	        let r = roll_die(size);
+			rolls.push(...r);
+		}
+    });
 	return rolls;
 }
 
-const respond = (dice, rolls, locale) => {
-	// build text response
-	let ones = rolls.reduce((c, r) => r === 1 ? c+1 : c, 0); 
-	let hits = rolls.reduce((c, r) => r >= 5 ? c+1 : c, 0); 
-	let resp = rolls.map(roll => `[${roll}]`).join(' ') + ' = ';
+const respond = (dice, rolls) => {
+	// build dice pool string
+	let resp = dice.map((sizeCount) => {
+		const [size, count] = sizeCount;
+		return count + 'D' + size;
+	}).join('+');
+	resp += ' <> ';
+	
+	// build rolls string
+	resp += rolls.join(' + ');
+	
+	// build total string
+	const total = rolls.reduce((acc, r) => { return acc + r }, 0);
+	resp += ' = ' + total;
 
-	// main SR4 logic
-	if ((dice - ones) <= (dice / 2)) {
-		if (hits == 0) resp += localResponses.criticalGlitch[locale];
-		else resp += hits + ' ' + localResponses.glitch[locale];
-	} else {
-		resp += hits + ' ' + localResponses.hits[locale];
-	}
 	return resp;
 }
 
@@ -48,63 +47,26 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('roll')
 		.setNameLocalization('hu', 'dobj')
-		.setDescription('Rolls Shadowrun 4 Anniversary Edition dice pools')
-		.setDescriptionLocalization('hu', 'kocka dobó a Shadowrun 4 Anniversary Edition szabályai szerint')
+		.setDescription('Rolls Earhdawn 4 Edition dice')
+		.setDescriptionLocalization('hu', 'Kockadobó az Earthdawn 4 kiadás szabályai szerint')
 		.addIntegerOption(option => 
-			option.setName('dice')
-				.setNameLocalization('hu', 'kockák')
+			option.setName('step')
+				.setNameLocalization('hu', 'fokozat')
 				.setRequired(true)
-				.setDescription('The number of dice')
-				.setDescriptionLocalization('hu', 'A kockák száma')
-		)
-		.addBooleanOption(option =>
-			option.setName('edge')
-				.setNameLocalization('hu', 'mázli')
-				.setRequired(false)
-				.setDescription('Did you use Edge?')
-				.setDescriptionLocalization('hu', 'Használtál Mázlit?')
+				.setDescription('Step')
+				.setDescriptionLocalization('hu', 'Fokozat')
 		),
 	async execute(interaction) {
 
-		// roll dice, count hits and ones
-		let dice = interaction.options.getInteger('dice');
-		const edge = interaction.options.getBoolean('edge') || false;
-		const rolls = roll(dice, edge);
-		const locale = interaction.locale.substring(0,2);
-		let resp = respond(dice, rolls, locale);
-
-		// build reroll button if needed
-		if (edge) {
-			interaction.reply({ content: resp });
-		} else {
-			const reroll = new ButtonBuilder()
-				.setCustomId('reroll')
-				.setLabel(localResponses.reroll[locale])
-				.setStyle(ButtonStyle.Primary);
-			const keep = new ButtonBuilder()
-				.setCustomId('keep')
-				.setLabel(localResponses.keep[locale])
-				.setStyle(ButtonStyle.Secondary);
-			const row = new ActionRowBuilder().addComponents(reroll, keep);
-			const response = await interaction.reply({ content: resp, components: [row] });
-
-			// reacting the reroll/keep button
-			const collectorFilter = i => i.user.id === interaction.user.id;
-			try {
-				const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 30_000 });
-				if (confirmation.customId === 'reroll') {
-					// rerolls start with the successes from the OG roll
-					rerolls = rolls.filter(r => r >= 5);
-					rerolls.push(...roll(rolls.length - rerolls.length, false));
-					resp = respond(dice, rerolls, locale);
-					await interaction.editReply({ content: resp, components: [] });
-				} else if (confirmation.customId === 'keep') {
-					await interaction.editReply({ content: resp, components: [] });
-				}
-			} catch (e) {
-				await interaction.editReply({ content: resp, components: [] });
-			}
+		// roll dice
+		const step = interaction.options.getInteger('step');
+		if (!data.steps[step]) {
+			interaction.reply({ content: 'Invalid step', ephemeral: true });
+			return;
 		}
-
+		dice = data.steps[step];
+		const rolls = roll_dice(dice);
+		let resp = respond(dice, rolls);
+		interaction.reply({ content: resp });
 	},
 };
